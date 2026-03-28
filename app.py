937dd333b499a94200e0416c3c2a23f49d88e854
PabloDavidVega <sebastiandresibarra@gmail.com>
2026-03-27 21:21:17 -0500

    Crud editar empleado

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import mysql.connector
 from mysql.connector import Error
 from flask import Flask, render_template, request, redirect, url_for
+from datetime import datetime
+import os 
 
 app = Flask (__name__)
+
+carpeta = os.path.join('uploads') #Creo la variable carpeta para manipular uploads
+app.config['carpeta'] = carpeta  #Crear una referencia de la ruta
 
 @app.route('/consulta')
 def index():
@@ -43,6 +48,11 @@ def almacenamiento():
     _nombre = request.form['txtnombre']
     _correo = request.form['txtcorreo']
     _foto = request.files['txtfoto']
+    now = datetime.now()
+    tiempo = now.strftime('%Y%H_%M_%S_')
+    if _foto.filename != '':
+        nuevonombreFoto = tiempo + _foto.filename
+        _foto.save('uploads/'+ nuevonombreFoto)
     try:
         #conexion
         conexion = mysql.connector.connect(
@@ -54,7 +64,7 @@ def almacenamiento():
         if conexion.is_connected():
             cursor1 = conexion.cursor()
             cursor1.execute(f"""insert into empleados (id_empleado, nombre_empleado, correo_empleado, imagen_empleado)
-                                values (null, '{_nombre}', '{_correo}', '{_foto.filename}');""")
+                                values (null, '{_nombre}', '{_correo}', '{nuevonombreFoto}');""")
             conexion.commit()
             print("Datos insertados con exito")
     except Error as e:
@@ -78,6 +88,9 @@ def eliminar(id): #Paso el parametro de la ruta al metodo
         )
         if conexion.is_connected():
             cursor1 = conexion.cursor()
+            cursor1.execute(f"""select imagen_empleado from empleados where id_empleado = {id};""")
+            fila = cursor1.fetchall()
+            os.remove(os.path.join(app.config['carpeta'], fila[0][0]))
             cursor1.execute(f"""delete from empleados where id_empleado= {id};""")
             conexion.commit()
     except Error as e:
@@ -87,6 +100,79 @@ def eliminar(id): #Paso el parametro de la ruta al metodo
             cursor1.close()
             conexion.close()
     return redirect('/consulta')
+
+@app.route('/editar/<int:id>')
+def editar(id):
+    
+    try:
+        #conexion
+        conexion = mysql.connector.connect(
+            host = 'localhost',
+            user = 'root',
+            passwd = '',
+            database = 'sistem'
+        )
+        if conexion.is_connected():
+            cursor1 = conexion.cursor()
+            cursor1.execute(f"""SELECT * FROM empleados where id_empleado = {id};""")
+            resultado = cursor1.fetchall() #sacar la informacion del objeto cursor y la pasa auna variable
+            conexion.commit()
+    except Error as e:
+        print('Error durante la conexion o ejecucion de la consola: ', e)
+    finally:
+        if conexion.is_connected():
+            cursor1.close()
+            conexion.close()
+    
+    return render_template('empleados/edit.html', datos=resultado)
+
+@app.route('/update', methods = ['POST'])
+def update():
+    _nombre = request.form['txtnombre']
+    _correo = request.form['txtcorreo']
+    _foto = request.files['txtfoto']
+    _id = request.form['txtID']
+    now = datetime.now()
+    tiempo = now.strftime('%Y%H_%M_%S_')
+ 
+    try:
+        #conexion
+        conexion = mysql.connector.connect(
+            host = 'localhost',
+            user = 'root',
+            passwd = '',
+            database = 'sistem'
+        )
+        if conexion.is_connected():
+            cursor1 = conexion.cursor()
+            #si me actualizaron la foto
+            if _foto.filename != '':
+                nuevonombreFoto = tiempo + _foto.filename
+                _foto.save('uploads/'+ nuevonombreFoto)
+                cursor1.execute(f"""select imagen_empleado from empleados where id_empleado = {id});""")
+                fila= cursor1.fetchall()
+                os.remove(os.path.join(app.config['carpeta'],fila [0][0]))
+                cursor1.execute(f"""update empleados set imagen_empleados = {nuevonombreFoto})
+                                where id_empleado ={_id};""")
+            
+                cursor1.commit
+            cursor1.execute(f"""update empleados set nombre_empleado ={nombre}, correo_empleado = {_correo}
+                            where id_empleado={_id};""")
+            
+            cursor1.commit()
+            
+        #Si no actualizaron la foto
+        cursor1.execute(f"""update empleados set nombre_empleado ={nombre}, correo_empleado = {_correo}
+                            where id_empleado={_id};""")
+            
+        conexion.commit()
+        print("Datos insertados con exito")
+    except Error as e:
+        print('Error durante la conexion o ejecucion de la consola: ', e)
+    finally:
+        if conexion.is_connected():
+            cursor1.close()
+            conexion.close()
 
 app.run(host='0.0.0.0', port=81, debug=True)
 
